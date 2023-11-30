28e0646e8aa8f63b632b49486dae90c47120b49d
Thiago <thiago___melo@hotmail.com>
2023-11-29 23:25:29 -0300

    adicionando hashmap

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -6,6 +6,8 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -17,7 +19,7 @@ public class PrincipalComListas {
         filme3.avalia(10);
         var serie1 = new Serie("Lost", 2000);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new ArrayList<>();
         lista.add(filme1);
         lista.add(filme2);
         lista.add(filme3);
@@ -38,6 +40,11 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtistas);
 
         Collections.sort(lista);
-        System.out.println("Lista de titulos ordenados: " + lista);
+        System.out.println("Lista de titulos ordenados: ");
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
     }
 }
